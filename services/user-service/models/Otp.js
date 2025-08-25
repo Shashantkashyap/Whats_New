4e3e2d5ad1172b0d103134bf5bf7ad0783115927
Shashant Kashyap <shashant.kashyap999@gmail.com>
2025-08-25 22:52:25 +0530

    Add purpose field to Otp model for enhanced OTP management

--- services/user-service/models/Otp.js
+++ services/user-service/models/Otp.js
@@ -6,7 +6,11 @@ const Otp = sequelize.define("Otp", {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   code: { type: DataTypes.STRING, allowNull: false },
   expiresAt: { type: DataTypes.DATE, allowNull: false },
-});
+purpose: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "signup", // ✅ default rakha
+  },});
 
 User.hasOne(Otp, { foreignKey: "userId", onDelete: "CASCADE" });
 Otp.belongsTo(User, { foreignKey: "userId" });
