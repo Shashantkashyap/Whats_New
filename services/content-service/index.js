b358877c91435de88049ea5acf9e82b835ada43d
Shashant Kashyap <shashant.kashyap999@gmail.com>
2025-08-29 20:30:31 +0530

    Initialize content service with Express and set up server connection

---
+++ services/content-service/index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const connectDB = require("./config/db");
+const app = require("./app");
+
+const PORT = process.env.PORT || 5000;
+
+// connect DB + start server
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+});
